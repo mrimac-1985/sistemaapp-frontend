c653df768e41a737c5b8407dd047d833efc171d7
mrimac-1985 <maximorimacayala@gmail.com>
2021-02-20 15:32:30 -0500

    ordenando codigo

--- src/app/util/notificacion.service.ts
+++ src/app/util/notificacion.service.ts
@@ -10,15 +10,24 @@ import { NotificacionComponent } from './notificacion/notificacion.component';
 export class NotificacionService {
 
   constructor(private sackbar: MatSnackBar ) { }
+ 
 
+  ruta_exito :string = "../../../assets/iconos/ok.png";
+  ruta_error :string = "../../../assets/iconos/error.png";
+  ruta_alera :string = "../../../assets/iconos/warning.png";
 
+  
   mostrarNotificacion(mensaje: string , buttonTexto :string , tipoMensaje : 'error'|'exito'|'alerta' ){
+
+
+    let ruta_imagen : string = tipoMensaje == 'exito' ? this.ruta_exito :  tipoMensaje == 'error' ? this.ruta_error :  tipoMensaje == 'alerta' ? this.ruta_alera :  '';
 
     this.sackbar.openFromComponent(NotificacionComponent, {
       data:{
         message:mensaje,
         buttonText:buttonTexto,
-        type: tipoMensaje
+        type: tipoMensaje,
+        rutaimage: ruta_imagen
       },
       duration : 5000,
       horizontalPosition:'right',
@@ -26,5 +35,9 @@ export class NotificacionService {
       panelClass: tipoMensaje
     });
   }
+
+
+
+
 
 }
